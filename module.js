d0f38438555d79816d46f190a86da7b1eedf66bf
realman <realman112358@163.com>
2014-10-31 18:21:59 +0800

    study test

---
+++ module.js
@@ -0,0 +1,7 @@
+/**
+ *
+ * Created by jw_hu on 2014/10/31.
+ */
+module.exports.func1 = function(){
+    console.log('fuck');
+};
---
+++ study.js
@@ -0,0 +1,7 @@
+/**
+ *
+ * Created by jw_hu on 2014/10/31.
+ */
+
+var module = require('./module');
+module.func1();
